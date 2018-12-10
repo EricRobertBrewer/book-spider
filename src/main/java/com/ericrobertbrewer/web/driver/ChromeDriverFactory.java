0f0831da018b230ad4c35cd9fd0d8b7cfeb5493f
Eric Brewer <eric.r.brewer@gmail.com>
2018-12-10 12:41:42 -0700

    Add documentation to ChromeDriverFactory.

--- src/main/java/com/ericrobertbrewer/web/driver/ChromeDriverFactory.java
+++ src/main/java/com/ericrobertbrewer/web/driver/ChromeDriverFactory.java
@@ -4,6 +4,16 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class ChromeDriverFactory implements WebDriverFactory {
 
+    /**
+     * Get a new instance of {@link ChromeDriver}.
+     * Be sure to set the {@code webdriver.chrome.driver} system property as the path to your local Chrome driver
+     * executable before invoking this method. There are two ways to do so:
+     * <ol>
+     *     <li>In the source code, using: {@code System.setProperty("webdriver.chrome.driver", path);}, or</li>
+     *     <li>As a VM argument to the program: {@code -Dwebdriver.chrome.driver=path}.</li>
+     * </ol>
+     * @return      A new instance of {@link ChromeDriver}.
+     */
     @Override
     public ChromeDriver newInstance() {
         return new ChromeDriver();
